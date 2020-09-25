daa51f9c947ba0043a732c4c27ee25742de41742
Rácz András István <randris99@gmail.com>
2020-09-25 22:05:56 +0200

    A grafikus felület főoldala elkészült.

--- src/main/java/controllers/MainWindowController.java
+++ src/main/java/controllers/MainWindowController.java
@@ -10,7 +10,7 @@ public class MainWindowController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("MainPage");
     }
 
 }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,7 +18,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("MainWindow"));
+        BorderPane root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        FXMLLoader listLoader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        root.setCenter(listLoader.load());
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
