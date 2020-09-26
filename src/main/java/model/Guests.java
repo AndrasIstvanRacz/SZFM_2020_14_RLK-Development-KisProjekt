6ec3896f934848525da3ee099f25597e746ea466
asder <lbalazs96@citromail.hu>
2020-09-26 16:00:04 +0200

    Az adatbázis változóinak a létrehozása.

--- src/main/java/model/Guests.java
+++ src/main/java/model/Guests.java
@@ -1,12 +1,45 @@
 package model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Date;
+
 @Data
+@Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table( name = "guests")
 public class Guests {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phonenumber")
+    private Integer phonenumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "startdate")
+    private Date startdate;
+
+    @Column(name = "enddate")
+    private Date enddate;
+
+    @Column(name = "roomtype")
+    private String roomtype;
+
+    @Column(name = "payment")
+    private Integer payment;
 }
