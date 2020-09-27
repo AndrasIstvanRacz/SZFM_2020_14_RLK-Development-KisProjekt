f984d2207b9a31b451923f5b35d6a7ca2abe928f
kzoltan99 <kzotya99@gmail.com>
2020-09-27 14:45:35 +0200

    GuestsRepository importok megírása

--- src/main/java/database/GuestsRepository.java
+++ src/main/java/database/GuestsRepository.java
@@ -1,4 +1,15 @@
 package database;
 
+import model.Guests;
+import org.tinylog.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuestsRepository {
 }
