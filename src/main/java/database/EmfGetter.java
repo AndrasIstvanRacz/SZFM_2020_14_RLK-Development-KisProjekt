17d8b85339f6154247cdd2a23b21d3f2fc7287d5
kzoltan99 <kzotya99@gmail.com>
2020-09-27 14:37:20 +0200

    EmfGetter céljának leírása

--- src/main/java/database/EmfGetter.java
+++ src/main/java/database/EmfGetter.java
@@ -4,4 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-
+/**
+ * This Class make the connection to the database.
+ */
