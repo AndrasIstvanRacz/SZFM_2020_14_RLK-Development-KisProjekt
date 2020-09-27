7ac1fc62a5dc6422c5adf0685d57b9532e6d4c20
kzoltan99 <kzotya99@gmail.com>
2020-09-27 14:41:52 +0200

    EmfGetter megírása

--- src/main/java/database/EmfGetter.java
+++ src/main/java/database/EmfGetter.java
@@ -7,3 +7,18 @@ import javax.persistence.Persistence;
 /**
  * This Class make the connection to the database.
  */
+public class EmfGetter {
+
+    private EmfGetter(){}
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("UsersDB");
+
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+
+    public static void closeEmf(){
+        emf.close();
+    }
+
+}
